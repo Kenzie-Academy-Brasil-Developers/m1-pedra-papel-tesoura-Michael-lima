a876e68a4294b6715691eba20e68bbe2d9df691a
Michael-lima <michaelkuhnedelima@gmail.com>
2023-09-02 12:19:36 -0300

    Entrega - 3 - Pedra-Papel-Tesoura

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 /* Desenvolva seu código abaixo */
+
+// Michael kuhne de lima
+
+function playRockPaperScissor(player1, player2) {
+
+    if (player1 === "Pedra" && player1 === "Papel" && player1 === "Tesoura" ||
+        player2 === "Pedra" && player2 === "Papel" && player2 === "Tesoura") {
+        return true;
+    }
+
+    if (player1 === player2) {
+        return "Empate!";
+
+    } else if (
+        (player1 === "Pedra" && player2 === "Tesoura") ||
+        (player1 === "Tesoura" && player2 === "Papel") ||
+        (player1 === "Papel" && player2 === "Pedra")
+    ) {
+
+        return "Jogador 1 venceu!";
+
+    } else {
+
+        return "Jogador 2 venceu!";
+    }
+
+
+}
+
